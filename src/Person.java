71273a8c1e02f3c06f6a1618b12b0a670a1eaf57
Abezer <abezes.haile@gmail.com>
2018-10-04 15:37:46 -0400

    Updated project

--- src/Person.java
+++ src/Person.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Person {
     public static Scanner input = new Scanner(System.in);
 
+
     public static void main(String[] args) {
 
         double balance;
         String YesOrNO;
-      //  Scanner input = new Scanner(System.in);
+        //  Scanner input = new Scanner(System.in);
         ArrayList<Account> myAccount = new ArrayList<>();
+        // creates a new accounts
         do {
 
 
@@ -36,71 +38,95 @@ public class Person {
         } while (YesOrNO.equalsIgnoreCase("Yes"));
 
 
+        // a user enters his account number and pin
+
+        int count=0;
+        //boolean
+
+        do {
+
+        Account account1 = new Account();
+
         System.out.println("Please Enter Your Account Number to Login:");
 
         String actNum = input.nextLine();
-
+        account1.setNumber(actNum);
         System.out.println("Please Enter Your PIN to Login: ");
 
         String pinNum = input.nextLine();
+        account1.setPin(pinNum);
+
 
         //after accepting pin
         // int i;
+
+
         //validation
 
 
-        for (int i = 0; i < myAccount.size(); i++) {
-            if (actNum.equals(myAccount.get(i).getNumber()) && pinNum.equals(myAccount.get(i).getPin())) {
-                balance = myAccount.get(i).getBalance();
-                System.out.print("\nPlease Choose From the Following Options:"
-                        + "\n 1. Display Balance \n 2. Deposit"
-                        + "\n 3. Withdraw\n 4. Log Out\n\n");
+            for (int i = 0; i < myAccount.size(); i++) {
 
-                int menuChoice = input.nextInt();
+                if (actNum.equals(myAccount.get(i).getNumber()) && pinNum.equals(myAccount.get(i).getPin())) {
+                    balance = myAccount.get(i).getBalance();
 
-                if (menuChoice < 1 || menuChoice > 4) {
-                    System.out.println("error");
-                } else if (menuChoice == 1) {
-                    displayBalance(myAccount.get(i).getBalance());
-                } else if (menuChoice == 2) {
+                    System.out.print("\nPlease Choose From the Following Options:"
+                            + "\n 1. Display Balance \n 2. Deposit"
+                            + "\n 3. Withdraw\n 4. Log Out\n\n");
 
-                } else if (menuChoice == 3) {
-                    withdraw(myAccount.get(i).getBalance());
-                } else if (menuChoice == 4) {
-                    System.out.println("Thank you for banking we us");
-                } else
-                    System.out.println("Invalid Input");
-            } else
-                System.out.println("Invalid Account number or PIN");
+                    int menuChoice = input.nextInt();
+
+                    if (menuChoice < 1 || menuChoice > 4) {
+                        System.out.println("error");
+                    } else if (menuChoice == 1) {
+                        displayBalance(balance);
+                    } else if (menuChoice == 2) {
+                        System.out.println("Enter the Amount you want to deposit !");
+                        double deposit = input.nextDouble();
+                        deposit(deposit, balance);
+
+                    } else if (menuChoice == 3) {
+                        System.out.println("enter the amount you want to withdraw ");
+                        double withmoney = input.nextDouble();
+                        withdraw(withmoney, balance);
+                    } else if (menuChoice == 4) {
+                        System.out.println("Thank you for banking we us");
+                    } else
+                        System.out.println("Invalid Input");
+                } else {
+                    System.out.println("Invalid Account number or PIN");
+                }
+
+            }
+            count++;
+
+        }while(count<3);
+
+        if(count==3){
+            System.out.println("Your Account is LOCKED !");
         }
-
-
     }
+
+
+
 
     public static void displayBalance(double x) {
         System.out.printf("\nYour Current Balance is $%.2f\n", x);
     }
 
-    public static void withdraw(double balance) {
-        System.out.println("Enter the amount you want to Withdraw");
+    public static void withdraw(double money,double balance) {
 
-
-        double withdraw = input.nextDouble();
-        if(withdraw>balance){
+        if(money>balance){
             System.out.println("Withdraw not processed Try different amount");
 
         }else
 
-        balance = balance - withdraw;
+        balance = balance - money;
         System.out.println("Your Remaining balance is:" + balance);
 
-
     }
-    public static void deposit(double money){
+    public static void deposit(double money,double balance){
 
-        double deposit = input.nextDouble();
-
-        money = money +deposit;
+        balance = balance +money ;
         System.out.println("Your balance is :" + money);
 
     }
